2ad4546852a0ef2900e192b94781643b20aac0aa
Arij Cheriet <cherietarij@gmail.com>
2026-02-15 19:48:44 +0100

    Version finale de la page d'accueil pour le 15/02

--- src/Controller/HomePageController.php
+++ src/Controller/HomePageController.php
@@ -5,38 +5,44 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
-class HomepageController extends AbstractController {
-  #[Route('/', name: 'cinema_homepage', methods: ['GET'])]
-  public function index(): Response
-
+class HomePageController extends AbstractController
 {
-   $films = [
-        [ id => 1, 
-        titre=> "Avatar 3: de feu et de cendres", 
-        poster=>,
-        duree=>,
-        note=>, 
-        seances =>
-
-           
+    #[Route('/', name: 'home')] // Attribute PHP 8 . Définit la route principale (home) du site. Execution de la méthode index()
+public function index(): Response 
+{
+    $films = [
+        [
+            'titre' => 'Inception',
+            'poster' => 'https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg',
+            'duree' => 148,
+            'note' => 5
         ],
         [
-            
+            'titre' => 'Interstellar',
+            'poster' => 'https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg',
+            'duree' => 169,
+            'note' => 4
         ],
+        [
+            'titre' => 'Joker',
+            'poster' => 'https://image.tmdb.org/t/p/w500/udDclJoHjfjb8Ekgsd4FDteOkCU.jpg',
+            'duree' => 122,
+            'note' => 5
+        ],
+        [
+            'titre' => 'The Batman',
+            'poster' => 'https://image.tmdb.org/t/p/w500/74xTEgt7R36Fpooo50r9T25onhq.jpg',
+            'duree' => 176,
+            'note' => 4
+        ]
     ];
 
- 
-    return $this->render('homepage.html.twig', [
+    return $this->render('homepage.html.twig', [ //méthode render avec twig qui gere l'affichage et le controleur les données 
         'films' => $films
     ]);
+}
 
 
-}}
-
-
-
-
-
-?>
+}
